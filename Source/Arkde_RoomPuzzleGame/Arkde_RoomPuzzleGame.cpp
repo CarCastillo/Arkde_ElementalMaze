a19cfbef36ea0a22e6bd7b628472c67a6e9f2e0f
CarCastillo <poleecastillo@gmail.com>
2020-08-02 11:20:44 -0500

    Create Unreal project - Let's go!

---
+++ Source/Arkde_RoomPuzzleGame/Arkde_RoomPuzzleGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Arkde_RoomPuzzleGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arkde_RoomPuzzleGame, "Arkde_RoomPuzzleGame" );
---
+++ Source/Arkde_RoomPuzzleGame/Arkde_RoomPuzzleGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Arkde_RoomPuzzleGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKDE_ROOMPUZZLEGAME_API AArkde_RoomPuzzleGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
